0a734d853fcb005cdafee77ebc4a06b988dc6ecd
Gio <paladino.gio@gmail.com>
2025-01-27 22:52:39 -0700

    Added slide feature

--- BigMode Runner/Assets/GioTest/Scripts/PlayerGio.cs
+++ BigMode Runner/Assets/GioTest/Scripts/PlayerGio.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerGio : MonoBehaviour
 {
     private Rigidbody rb;
+    private CapsuleCollider col;
 
     [SerializeField]
     private float maxForwardSpeed;
@@ -13,17 +14,57 @@ public class PlayerGio : MonoBehaviour
     [SerializeField]
     private float forwardforce;
 
+    [SerializeField]
+    private float slideStopForce;
+    private bool sliding = false;
+    private bool tryToStopSliding = false;
+
+    [SerializeField]
+    private Transform CeilingCheckCastOrigin;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        ToggleSlide();
         ApplyForwardForce();
         HorizontalMovement();
+        
+    }
+
+    private void ToggleSlide()
+    {
+        if (!sliding && Input.GetKeyDown(KeyCode.S))
+        {
+            col.height = col.height / 2f;
+            col.center += Vector3.down * 0.25f;
+            sliding = true;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            tryToStopSliding = true;
+        }
+
+        if (tryToStopSliding)
+        {
+            Debug.Log("tryToStopSliding = " + tryToStopSliding);
+            Debug.DrawRay(CeilingCheckCastOrigin.position, Vector3.up * 2f, Color.red, 2f, false);
+            RaycastHit hit;
+            if(!Physics.Raycast(CeilingCheckCastOrigin.position, Vector3.up, out hit, 1f))
+            {
+                col.height = col.height * 2f;
+                col.center += Vector3.up * 0.25f;
+                sliding = false;
+                tryToStopSliding = false;
+            }
+        }
     }
 
     private void ApplyForwardForce()
@@ -34,14 +75,19 @@ public class PlayerGio : MonoBehaviour
 
     private void HorizontalMovement()
     {
-            if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && !sliding)
         {
             rb.AddForce(transform.TransformDirection(Vector3.left) * sideForce * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && !sliding)
         {
             rb.AddForce(transform.TransformDirection(Vector3.right) * sideForce * Time.deltaTime);
+        }
+
+        if (sliding)
+        {
+           rb.AddForce(new Vector3(-rb.linearVelocity.x, 0f, 0f).normalized * slideStopForce * Time.deltaTime, ForceMode.Acceleration);
         }
     }
 }
